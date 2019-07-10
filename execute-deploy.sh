13ea420df97e6bba24f71c6ec0dad2f5b67ef2c6
academey <academey@gmail.com>
2019-07-10 11:48:54 +0900

    Add demon to docker-compose

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 cd /home/ec2-user/ridi-new-entrant-web
 sudo docker-compose stop
 sleep 10
-sudo docker-compose -f docker-compose.prod.yml up --build
+sudo docker-compose -d -f docker-compose.prod.yml up --build
